c2bc30a9858e011de78ad9da9829d28f4606dd0c
sloth-89 <restvoice2020@gmail.com>
2023-03-25 10:44:24 +0900

    230325

--- src/Game.js
+++ src/Game.js
@@ -9,9 +9,6 @@ import "./game.css";
 // console.log(squares2) // [null, null, null, null, null, null, null, null, null]
 // console.log(squares3) // ["데이터값", "데이터값", "데이터값", "데이터값", "데이터값", "데이터값", "데이터값", "데이터값", "데이터값"]
 
-function dsajfd(){
-    
-}
 // 게임 상단 문구
 function getStatus(squares, xIsNext){
     const winner = calculateWinner(squares);
